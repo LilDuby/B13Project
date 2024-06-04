dea9b8500d5b5af9bb9af7edb1ea6cc395fc5265
Changil Cho <chotogi98@gmail.com>
2024-06-04 20:01:05 +0900

    [Add] OpenDoor Animation
    문 열기 애니메이션 만들기

--- Assets/Scripts/Room4/Fall.cs
+++ Assets/Scripts/Room4/Fall.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 
 public class Fall : MonoBehaviour
 {
+    public int openNum = 0;
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Bottom Floor")
         {
             transform.position = new Vector3(13f, 0.15f, -47f);
+        }
+        else if (collision.gameObject.tag == "OpenFloor")
+        {
+            openNum = 5;
         }
     }
 }
--- Assets/Scripts/Room4/OpenDoor.cs
+++ Assets/Scripts/Room4/OpenDoor.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class OpenDoor : MonoBehaviour
 {
-    public GameObject Door;
+    public Fall fall;
 
-    private void OnCollisionEnter(Collision collision)
+    //public GameObject Door;
+
+    Animator animator;
+
+    private void Awake()
     {
-        if (collision.gameObject.tag == "OpenFloor")
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (fall.openNum == 5)
         {
-            Door.transform.rotation = Quaternion.Euler(new Vector3(0f, 80f, 0f));
+            animator.SetInteger("Open", 5);
         }
     }
+
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    if (collision.gameObject.tag == "OpenFloor")
+    //    {
+    //        //Door.transform.rotation = Quaternion.Euler(new Vector3(0f, 80f, 0f));
+    //        animator.SetInteger("Open", 5);
+    //    }
+    //}
 }
