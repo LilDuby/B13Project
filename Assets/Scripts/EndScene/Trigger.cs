352cfbbb16ff97b05f1d613a046a0e09de50e208
Kimmochy <roekdk@naver.com>
2024-06-07 20:21:11 +0900

    fix:endScene trigger

---
+++ Assets/Scripts/EndScene/Trigger.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Trigger : MonoBehaviour
+{ 
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<Player>() != null)
+        EndGame();
+    }
+
+    public void EndGame()
+    {
+        SceneManager.LoadScene("EndScene");
+    }   
+}
+
