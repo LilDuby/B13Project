db46bf61dfd44fdfad79b7af16c18aed6879b5aa
Changil Cho <chotogi98@gmail.com>
2024-06-07 15:12:53 +0900

    Update Lamp..

--- Assets/Prefabs/Eunoh/JumpRoomButton.cs
+++ Assets/Prefabs/Eunoh/JumpRoomButton.cs
@@ -6,12 +6,14 @@ public class JumpRoomButton : MonoBehaviour
 {
     public JumpRoomButtonData buttonData;
     public GameObject keyNumber;
+    public GameObject openRoom4Door;
 
     private void OnCollisionEnter(Collision collision)
     {
         if (buttonData.buttonType == ButtonType.Right)
         {
             keyNumber.SetActive(true);
+            openRoom4Door.transform.rotation = Quaternion.Euler(0f, -110f, 0f);
         }
         else
         {
